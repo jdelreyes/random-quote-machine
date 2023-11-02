79366f97941db54df20a86b6b0dfa7c78e7f6ab8
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2023-11-02 17:42:27 -0400

    refactored ui using custom css, app fetches random image and quote

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [quote, setQuote] = useState(null);
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const fetchRandomQuote = () => {
+    setLoading(true);
+    // const apiUrl = '';
     const apiUrl = "https://api.quotable.io/quotes/random";
 
-    fetch(apiUrl)
+    fetch(apiUrl, {
+      method: 'GET'
+    })
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-        return response.json();
+        return response.json(); // This returns a Promise
       })
       .then((data) => {
-        setQuote(data.content);
+        data.forEach((response) => {
+          setQuote(response.content);
+          setAuthor(response.author);
+        });
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
 
@@ -27,35 +38,54 @@ function App() {
   }, []);
 
   return (
-    <section
-      id="quote-box"
-      className="mx-auto container d-flex flex-column text-center border border-primary-subtle mw-60 rounded"
-    >
-      <div className="d-flex flex-column">
-        <h1 id="text" className="">
-          "{quote}"
-        </h1>
-        <p id="author" className="opacity-50 fst-italic">
-          author
-        </p>
-      </div>
-      <div className="d-flex flex-column">
-        <button
-          id="new-quote"
-          onClick={fetchRandomQuote}
-          className="btn btn-primary"
-        >
-          Generate New Quote
-        </button>
-        <a
-          id="tweet-quote"
-          href="https://twitter.com/intent/tweet"
-          target="_blank"
-        >
-          Tweet
-        </a>
-      </div>
-    </section>
+    <div style={{
+      backgroundImage: 'url("https://picsum.photos/50/75?blur")',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover'
+    }} className="d-flex flex-grow-1 justify-content-center align-items-center">
+      <section
+        id="quote-box"
+        className="p-5 border-0 rounded-4 m-2"
+      >
+        <div className="d-flex flex-column">
+          {loading ? <div class="spin-border m-5 mx-auto" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+            : <>
+              <header id="text" className="h4">
+
+                <h1 className="fw-bolder text-black">
+                  <span className="quotation-mark">"</span>&nbsp;&nbsp;&nbsp;{quote}
+                </h1>
+              </header>
+              <h3 id="author" className="opacity-75 fst-italic text-right mt-5">- {author}&nbsp;&nbsp;&nbsp;</h3>
+            </>}
+        </div>
+        <hr />
+        <div className="d-flex flex-column">
+          <button
+            id="new-quote"
+            onClick={fetchRandomQuote}
+            className="btn btn-outline-dark btn-lg"
+          >
+            Generate
+          </button>
+          <a
+            id="tweet-quote"
+            href={`https://twitter.com/intent/tweet?text=${quote}\n\n-${author}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+          </a>
+        </div>
+        <hr />
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </section >
+    </div >
   );
 }
 
